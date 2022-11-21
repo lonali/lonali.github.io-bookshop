a55260515c3aece5763ad858713e68a10d0ef866
lonali <ibragimova.ilona@gmail.com>
2022-11-22 06:40:02 +0900

    feat: add js files for cart and index

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let bshopItemsData = [{
+    id: "book1",
+    name: "Nightbitch",
+    author: "Rachel Yoder",
+    price: 112,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa omnis aliquid quo praesentium consectetur perferendis aspernatur amet sed unde ad. Iure quos fugit nobis suscipit obcaecati dolorem aperiam atque.",
+    img: "images/img-1.png"
+
+}, {
+    id: "book2",
+    name: "Billy Summers",
+    author: "Stephen King",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa omnis aliquid quo praesentium consectetur perferendis aspernatur amet sed unde ad. Iure quos fugit nobis suscipit obcaecati dolorem aperiam atque.",
+    img: "images/img-2.png"
+}, {
+    id: "book3",
+    name: "The 1619 Project",
+    author: "Nikole H. Jones",
+    price: 34,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa omnis aliquid quo praesentium consectetur perferendis aspernatur amet sed unde ad. Iure quos fugit nobis suscipit obcaecati dolorem aperiam atque.",
+    img: "images/img-3.png"
+}, {
+    id: "book4",
+    name: "I love you but I've chosen darkness",
+    author: "Claire V. Watkins",
+    price: 76,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa omnis aliquid quo praesentium consectetur perferendis aspernatur amet sed unde ad. Iure quos fugit nobis suscipit obcaecati dolorem aperiam atque.",
+    img: "images/img-4.png"
+}];
